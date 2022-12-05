eb683ce100500c5251f95b3be493f9ee2ccc4cac
ilike3p <39431924+ilike3p@users.noreply.github.com>
2022-12-05 00:38:14 -0800

    added function

--- Develop/script.js
+++ Develop/script.js
@@ -15,9 +15,41 @@ passwordLength = prompt("Choose how many characters you would like in password")
 if (passwordLength<8){
 alert ("password length must be between 8-128 characters");
 determineLenght();
-} else if ("passwordLength>128"){
-
+}else if (passwordLength>128){
+  alert("Password length must be a number between 8-128 characters");
+  determineLength();
+}else if (isNaN(passwordLength)){
+  alert("Password length must be a number between 8-128 characters");
+  determineLength();
+}else{
+alert("The next three screens will ask you the types of characters you would like to be included in your password.If you choose 'No' for all, your password will only contain lowercase letters.");
 }
+return passwordLength;
+
+function determineUppercase(){
+  uppercaseCheck = prompt("Do you want to include uppercase in your password?(Yes or No)");
+    uppercaseCheck = uppercaseCheck.toLowerCase();
+
+    if (uppercaseCheck === null || uppercaseCheck === ""){
+      alert("Please answer Yes or No");
+      determineUppercase();
+
+    }else if (uppercaseCheck === "yes" || uppercaseCheck ==="y"){
+      uppercaseCheck = true;
+      return uppercaseCheck;
+
+    }else if (uppercaseCheck === "no" || uppercaseCheck ==="n"){
+      uppercaseCheck = false;
+      return uppercaseCheck;
+    
+    }else {
+      alert("Please answer Yes or No");
+      determineUppercase();
+    }
+    return uppercaseCheck;
+}
+
+
 // Write password to the #password input
 
 function writePassword() {
@@ -26,13 +58,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}else if (isNaN(passwordLength)){
-  alert("Password length must be a number between 8-128 characters");
-  determineLength();
-}else{
-alert("The next three screens will ask you what types of characters you would like to be included in your password.\nIf you choose 'No' for all, your password will only contain lowercase letters.");
 }
-return passwordLength;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
